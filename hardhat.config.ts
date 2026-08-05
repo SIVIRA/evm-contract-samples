c7fdeb65e41089606fc26dd87a8886811132432f
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2026-08-05 20:24:27 +0900

    build: update dev deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,6 @@
-import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
 import { defineConfig } from "hardhat/config";
 
 export default defineConfig({
-  plugins: [hardhatToolboxViemPlugin],
   solidity: {
     version: "0.8.35",
     settings: {
